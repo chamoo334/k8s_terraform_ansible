bd602f73a6a5cf94eeef0cd6ace7348c715327b6
cm-pipeline-at-797918408294 <cm@Mac.local>
2023-04-13 23:41:28 -0700

    AWS key pair, security groups, and ec2 instances completed. Associated file resources for private key and hostnames completed

--- modules/aws/ec2.tf
+++ modules/aws/ec2.tf
@@ -8,8 +8,8 @@ resource "aws_instance" "k8s" {
     ami           = var.ami_id
     instance_type = var.instance_type
     associate_public_ip_address = true
-    # key_name = aws_key_pair.l8s.key_name
-    # security_groups = [""]
+    key_name = aws_key_pair.k8s.key_name
+    security_groups = each.key == "controller" ? [aws_security_group.k8s-controller.name] : [aws_security_group.k8s-worker.name]
     
     tags = {
         Name = each.value
